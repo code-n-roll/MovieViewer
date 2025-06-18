c8c15aa4067448831cdfa5a2112f4f3c08fb97ef
roman.karanchuk <romankaranchuk@gmail.com>
2025-06-19 01:43:49 +0200

    Add const for ApiConstants

--- app/src/main/java/com/karanchuk/movieviewer/data/source/network/ApiConstants.kt
+++ app/src/main/java/com/karanchuk/movieviewer/data/source/network/ApiConstants.kt
@@ -3,7 +3,7 @@ package com.karanchuk.movieviewer.data.source.network
 import com.karanchuk.movieviewer.BuildConfig
 
 object ApiConstants {
-    val API_KEY = BuildConfig.API_KEY
-    val BASE_URL = "https://api.themoviedb.org/3/"
-    val POSTER_BASE_URL = "https://image.tmdb.org/t/p/w185"
+    const val API_KEY = BuildConfig.API_KEY
+    const val BASE_URL = "https://api.themoviedb.org/3/"
+    const val POSTER_BASE_URL = "https://image.tmdb.org/t/p/w185"
 }
