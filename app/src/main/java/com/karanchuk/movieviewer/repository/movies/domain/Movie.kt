831297e890402de5970b6c5d6d799ae25357ac94
roman.karanchuk <romankaranchuk@gmail.com>
2025-06-19 01:46:00 +0200

    Clean up Movie

--- app/src/main/java/com/karanchuk/movieviewer/repository/movies/domain/Movie.kt
+++ app/src/main/java/com/karanchuk/movieviewer/repository/movies/domain/Movie.kt
@@ -8,14 +8,4 @@ data class Movie(
     val posterUrl: String,
     val voteAverage: Double,
     val releaseDate: LocalDate,
-) {
-    companion object {
-        val Preview = Movie(
-            id = 0,
-            title = "The Shawshank Redemption",
-            posterUrl = "https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1",
-            voteAverage = 9.2,
-            releaseDate = LocalDate.of(1994, 9, 23),
-        )
-    }
-}
+)
